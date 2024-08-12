d1cf1bf8c5ed6c186737eb82d76030f0a5c7064c
Pichborith Kong <pichborith@gmail.com>
2024-08-11 21:01:17 -0700

    add spring jpa and create database schema

---
+++ spring-security-jwt-demo/src/main/java/dev/pichborith/models/User.java
@@ -0,0 +1,5 @@
+package dev.pichborith.models;
+
+public class User {
+
+}
---
+++ spring-security-jwt-demo/src/main/resources/schema.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS users;
+
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    password VARCHAR(255) NOT NULL,
+    role VARCHAR NOT NULL DEFAULT 'USER'
+);
