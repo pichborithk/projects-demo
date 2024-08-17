a798181324da0046b1aa1961946b329156702171
Pichborith Kong <pichborith@gmail.com>
2024-08-16 20:34:38 -0700

    fixed notification not send on order confirmation

--- spring-microservices-demo/services/notification/src/main/java/dev/pichborith/ecommerce/kafka/NotificationConsumer.java
+++ spring-microservices-demo/services/notification/src/main/java/dev/pichborith/ecommerce/kafka/NotificationConsumer.java
@@ -59,12 +59,12 @@ public class NotificationConsumer {
         );
 
         var customerName = orderConfirmation.customer().firstname() + " " + orderConfirmation.customer().lastname();
-//        emailService.sendOrderConfirmationEmail(
-//            orderConfirmation.customer().email(),
-//            customerName,
-//            orderConfirmation.totalAmount(),
-//            orderConfirmation.orderReference(),
-//            orderConfirmation.products()
-//        );
+        emailService.sendOrderConfirmationEmail(
+            orderConfirmation.customer().email(),
+            customerName,
+            orderConfirmation.totalAmount(),
+            orderConfirmation.orderReference(),
+            orderConfirmation.products()
+        );
     }
 }
