7b042aa4fa3f279024ee0206a74869a0a6b8a084
Pichborith Kong <pichborith@gmail.com>
2024-08-28 10:54:18 -0700

    initial spring api

---
+++ stripe-checkout-demo/api/src/main/java/dev/pichborith/StripeCheckoutDemo/StripeCheckoutDemo.java
@@ -0,0 +1,13 @@
+package dev.pichborith.StripeCheckoutDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeCheckoutDemo {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeCheckoutDemo.class, args);
+	}
+
+}
---
+++ stripe-checkout-demo/api/src/test/java/dev/pichborith/StripeCheckoutDemo/StripeCheckoutDemoTests.java
@@ -0,0 +1,13 @@
+package dev.pichborith.StripeCheckoutDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripeCheckoutDemoTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
