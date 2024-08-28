1b4504e1477b2b8e3042eee4d1160518b2a6e7fb
Pichborith Kong <pichborith@gmail.com>
2024-08-28 10:55:35 -0700

    add react-router package and setup route provider

--- stripe-checkout-demo/client/src/main.tsx
+++ stripe-checkout-demo/client/src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Checkout from './Checkout.tsx';
+import Canceled from './Canceled.tsx';
+
+const router = createBrowserRouter([
+  { path: '/', element: <App /> },
+  { path: 'checkout', element: <Checkout /> },
+  { path: 'canceled', element: <Canceled /> },
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
