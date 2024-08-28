9b9f736ddb1ce1e05074db452d27d91c113e49ba
Pichborith Kong <pichborith@gmail.com>
2024-08-28 07:44:04 -0700

    setup checkout handling function

--- stripe-checkout-demo/client/src/App.tsx
+++ stripe-checkout-demo/client/src/App.tsx
@@ -1,9 +1,24 @@
 import './App.css';
 
 function App() {
+  const cart = {};
+
+  async function handlingCheckOut() {
+    const response = await fetch('http://localhost:1337/checkout', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ cart }),
+    });
+    const result = await response.json();
+    console.log(result);
+  }
+
   return (
     <>
       <h1>Home</h1>
+      <button onClick={handlingCheckOut}>Checkout</button>
     </>
   );
 }
